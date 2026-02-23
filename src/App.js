8fab5e8b69d2226855cb99b62e45ce0e3afdd09a
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2026-02-23 14:49:26 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import LoginPage from "./pages/LoginPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import CreateAccount from "./pages/CreateAccount";
+import AllTherapistsPage from "./pages/AllTherapistsPage";
+import UserPage from "./pages/UserPage";
+import TherUserPage from "./pages/TherUserPage";
 
 function App() {
     return (
@@ -12,6 +18,13 @@ function App() {
 function RoutesComponent() {
     return (
         <Routes>
+            {/*<Route path="/login" element={<LoginPage/>}/>*/}
+            <Route path="/forgot-password" element={<ResetPasswordPage/>}/>
+            <Route path="/create-account" element={<CreateAccount/>}/>
+            <Route path="/all-therapists" element={<AllTherapistsPage/>}/>
+            <Route path="/my-account/:id" element={<UserPage/>}/>
+            <Route path="/my-doc-account/:id" element={<TherUserPage/>}/>
+            {/*<Route path="/" element={<Navigate to="/login"/>}/>*/}
         </Routes>
     );
 }
